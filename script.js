c00d7696e9fff3450843035c1b91b7bd1c9f2940
Awadhesh Kumar Singh <118311133+Awadhesh0604@users.noreply.github.com>
2023-05-16 20:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+let yourArray;
+// assign your array the correct value in the next line
+// your code here!
+// code end
+yourArray = ["Acciojob", " Abushama",true,1,5];
+
+module.exports = yourArray
